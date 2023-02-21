6b2c1fe21db35b32e21f3ff4aec57bcd7a780991
でんのこ <111192883+dennnoko@users.noreply.github.com>
2023-02-21 17:11:48 +0900

    complete

--- app/src/main/java/com/example/a2023springviewapp/MainActivity.kt
+++ app/src/main/java/com/example/a2023springviewapp/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.a2023springviewapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.RecyclerView
+import io.realm.Realm
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,5 +17,45 @@ class MainActivity : AppCompatActivity() {
         adapter = MemoListAdapter()
         val recyclerView = findViewById<RecyclerView>(R.id.memo_list)
         recyclerView.adapter = adapter
+
+        val editText = findViewById<EditText>(R.id.memo_edit_text)
+        val addButton = findViewById<Button>(R.id.add_button)
+
+        val realm = Realm.getDefaultInstance()
+
+        addButton.setOnClickListener {
+            val text = editText.text.toString()
+            if(text.isEmpty()) {
+                return@setOnClickListener
+            }
+            //Realmのトランザクション
+            realm.executeTransactionAsync {
+                //Memoのオブジェクトを作成
+                val memo = it.createObject(Memo::class.java)
+                //Memoに入力してあったtextを代入
+                memo.name = text
+                //上書きする
+                it.copyFromRealm(memo)
+            }
+            //テキストを空にする
+            editText.text.clear()
+        }
+        //DBに変更があった時に通知が来る
+        realm.addChangeListener {
+            //変更があった時にリストをアップデートする
+            val memoList = it.where(Memo::class.java).findAll().map { it.name }
+            //UIスレッドで更新する
+            recyclerView.post {
+                adapter.updateMemoList(memoList)
+            }
+            //初回表示時にリストを表示
+            realm.executeTransactionAsync {
+                val memoList = it.where(Memo::class.java).findAll().map { it.name }
+                //UIスレッドで更新する
+                recyclerView.post {
+                    adapter.updateMemoList(memoList)
+                }
+            }
+        }
     }
 }
