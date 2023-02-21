4f99817c4f42cad040f0f32b6773844a872fa16a
でんのこ <111192883+dennnoko@users.noreply.github.com>
2023-02-21 16:47:40 +0900

    layout generate

--- app/src/main/java/com/example/a2023springviewapp/MainActivity.kt
+++ app/src/main/java/com/example/a2023springviewapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.a2023springviewapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: MemoListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        adapter = MemoListAdapter()
+        val recyclerView = findViewById<RecyclerView>(R.id.memo_list)
+        recyclerView.adapter = adapter
     }
 }
